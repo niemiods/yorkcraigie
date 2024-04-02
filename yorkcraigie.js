0469a58fa9b8847827b3d8a56bd995fc5fd9e77b
noss <mikaelo858818@gmail.com>
2024-04-02 16:06:37 +0800

    ad

---
+++ yorkcraigie.js
@@ -0,0 +1,43 @@
+
+import axios from 'axios';
+import { axiosConfig } from '@yorkcraigie/proxy';
+import { sleep } from '@yorkcraigie/sleep';
+import { formatTimestamp } from '@yorkcraigie/date-time-processor';
+import { notify } from 'feishu-notifier';
+import { log } from '@yorkcraigie/log';
+
+// simple_note_app.js
+
+class Note {
+    constructor(title, content) {
+        this.title = title;
+        this.content = content;
+    }
+}
+
+class NoteApp {
+    constructor() {
+        this.notes = [];
+    }
+
+    addNote(title, content) {
+        const note = new Note(title, content);
+        this.notes.push(note);
+    }
+
+    editNoteTitle(index, newTitle) {
+        this.notes[index].title = newTitle;
+    }
+
+    deleteNote(index) {
+        this.notes.splice(index, 1);
+    }
+}
+
+// Example usage
+const noteApp = new NoteApp();
+noteApp.addNote('Shopping List', '1. Milk\n2. Bread\n3. Eggs');
+noteApp.editNoteTitle(0, 'Grocery List');
+noteApp.deleteNote(0);
+console.log(noteApp.notes);
+
